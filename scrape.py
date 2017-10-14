06f1ef4138e211958627dafcab86ca344e44fc78
wizbright <wizbrighto@gmail.com>
2017-10-14 16:05:28 -0500

    still hates unicode

--- scrape.py
+++ scrape.py
@@ -8,44 +8,50 @@ import subprocess
 
 def main(arg1, arg2):
 
-	page = requests.get(arg1)
-	tree = html.fromstring(page.content)
+    page = requests.get(arg1)
+    tree = html.fromstring(page.content)
 
 #	print(arg1)
 
-	headline = tree.xpath('//header[@class="content-header "]//text()')
-	textContent = tree.xpath('//div[@class="content-text"]//text()')
+    headline = tree.xpath('//header[@class="content-header "]//text()')
+    textContent = tree.xpath('//div[@class="content-text"]//text()')
 
-	# clean up content
-	ht = [a.strip() for a in headline]
-	tct = [a.strip() for a in textContent]
+    # clean up content
+    ht = [a.strip() for a in headline]
+    tct = [a.strip() for a in textContent]
 
-	for x in ht:
-	    if x == "":
-	        ht.remove(x)
+    for x in ht:
+        if x == "":
+            ht.remove(x)
 
-	for x in tct:
-	    if x == "":
-	        tct.remove(x)
+    htencoded = ht
+    for line in htencoded:
+        line.encode("ascii", "ignore")
+
+    print(ht)
+    print(htencoded)
+    for x in tct:
+        if x == "":
+            tct.remove(x)
 
 #	print(ht)
 #	print(tct)
 
-	# Write to file
-	fmode = "a"
-	category = arg2
+    # Write to file
+    fmode = "a"
+    category = arg2
 
-	out = subprocess.run(["mkdir", category], stderr = subprocess.DEVNULL)
+    out = subprocess.run(["mkdir", category], stderr = subprocess.DEVNULL)
 
-	hf = open(category + "/headers.txt", fmode)
-	for item in ht:
-	    hf.write("%s\n" % item)
-	hf.close()
+    hf = open(category + "/headers.txt", fmode)
+    for item in ht:
+        hf.write("%s\n" % item)
+    hf.close()
 
-	cf = open(category + "/content.txt", fmode)
-	for item in tct:
-	    cf.write("%s\n" % item)
-	cf.close()
+    cf = open(category + "/content.txt", fmode)
+    for item in tct:
+        cf.write("%s\n" % item)
+    cf.close()
 
 if __name__ == "__main__":
-	main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2])
